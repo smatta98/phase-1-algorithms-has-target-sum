ff8122d44d5fe2c2a986be31889d61f9189db6b7
Shiv Matta <shiv.matta@gmail.com>
2022-07-20 21:04:28 -0500

    commit tree 4b72a81b925de176a725c4298bb57f7fe44fd0f5

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
+  for(i = 0; i< array.length; i++){
+    const something = target - array[i]
+    const complement = target - array[i];
+    // iterate over the remaining numbers in the array
+    for (let j = i + 1; j < array.length; j++) {
+      // check if any of the remaining numbers is the complement
+      // if so, return true
+      if (array[j] === complement) return true;
+    }
+  }
+  // if we reach the end of the array, return false
+  return false;
+  }
+
 
 /* 
   Write the Big O time complexity of your function here
